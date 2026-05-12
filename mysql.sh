5cf933fc0dcfee4e7638df556f54b48b120f0f84
LokeshThokala123 <lokeshibmi@gmail.com.com>
2026-05-12 16:46:20 +0530

    catalogue

---
+++ mysql.sh
@@ -0,0 +1,20 @@
+source common.sh
+
+echo -e "${hs} install mysql server ${he}" | tee -a ${Log_file}
+dnf install -y mysql8.4-server &>>${Log_file}
+status_check
+
+echo -e "${hs} Start MySQL Server ${he}" | tee -a ${log_file}
+systemctl enable mysqld &>>${log_file}
+systemctl start mysqld &>>${log_file}
+status_check
+
+echo -e "${hs} Setup MySQL Root Password ${he}" | tee -a ${log_file}
+mysql -u root -e "
+  CREATE USER 'root'@'%' IDENTIFIED BY 'RoboShop@1';
+  GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+  ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';
+  FLUSH PRIVILEGES;
+" &>>${log_file}
+status_check
+
